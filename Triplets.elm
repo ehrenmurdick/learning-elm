a445f9b07e022d943d3baf33837c7b14d28b60c8
Ehren Murdick <pair+emurdick@pivotallabs.com>
2017-11-01 21:07:28 -0700

    triangles!

--- Triplets.elm
+++ Triplets.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Svg exposing (Svg, svg, polygon)
+import Svg.Attributes exposing (points)
 import Complex exposing (..)
 
 
@@ -40,21 +42,52 @@ parseInput a s =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model (0 :+ 0), Cmd.none )
+    ( Model (4 :+ 3), Cmd.none )
 
 
-numberInput msg =
-    input [ attribute "type" "number", onInput msg ] []
+numberInput v msg =
+    input [ value (toString v), attribute "type" "number", onInput msg ] []
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ numberInput (parseInput SetReal)
-        , numberInput (parseInput SetImag)
-        , text (toString (square model.n))
-        , text (toString (sqMag model.n))
-        ]
+    let
+        n =
+            square model.n
+    in
+        div [ style [ ( "padding", "3em" ) ] ]
+            [ triangle model
+            , br [] []
+            , numberInput model.n.realPart (parseInput SetReal)
+            , numberInput model.n.imagPart (parseInput SetImag)
+            , text (toString [ n.realPart, n.imagPart, sqMag model.n ])
+            ]
+
+
+triangle : Model -> Svg Msg
+triangle model =
+    let
+        scale =
+            1
+
+        n =
+            square model.n
+
+        x =
+            ((scale * 500) - n.realPart) - 250
+
+        y =
+            n.imagPart + 250
+    in
+        svg [ height 500, width 500 ]
+            [ polygon [ points ("250,250 250," ++ (toString x) ++ " " ++ (toString y) ++ ",250") ] []
+            ]
+
+
+
+-- <svg height="210" width="500">
+--   <polygon points="200,10 250,190 160,210" style="fill:lime;stroke:purple;stroke-width:1" />
+-- </svg>
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
